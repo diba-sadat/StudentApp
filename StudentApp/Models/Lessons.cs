555c4e608ba962899fbcbd115c5110d1112f416d
Eigenaar <Eigenaar@LAPTOP-1D60MDVE>
2026-02-06 17:08:26 +0100

    finished lesson func

---
+++ StudentApp/Models/Lessons.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentApp.Models
+{
+    public class Lessons
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string Name { get; set; }
+
+    }
+}
